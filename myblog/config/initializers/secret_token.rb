235ffa34c6c23383a7910c4843474366a5c40363
Sneha Pulijal <Sneha@Snehas-MacBook-Pro.local>
2013-07-17 10:03:58 -0400

    first commit

---
+++ myblog/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Myblog::Application.config.secret_key_base = '2a2f583d7316045899b66333812926ea5f82c4228fb9282fd6035ace36af5b726d7d733437129a3a91b43dd5f4fe076cbfd504ce189418f7e4bafdf6fab87698'
---
+++ ronr1/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ronr1::Application.config.secret_key_base = 'e7fe1841d92f6bbaf77a0d6d0a456f710eec435acd04cb1cc82a9180790f6f81e33737eeb931f6a2687404516b4469e5d7607259be982afdacbb49dd66625a65'
